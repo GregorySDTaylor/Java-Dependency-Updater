1435a382c9b3fce0060a2c510204b3c55203eaba
Greg Taylor <GregorySDTaylor@gmail.com>
2021-12-11 15:40:43 -0500

    initialized gradle project

---
+++ app/src/main/java/me/gregorysdtaylor/dependencyupdater/App.java
@@ -0,0 +1,11 @@
+package me.gregorysdtaylor.dependencyupdater;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
